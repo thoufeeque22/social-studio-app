e6ce393487cb8b906e4e00b8ef68adfee0a3d85b
thoufeeque <thoufeeque.rafique@gmail.com>
2026-04-24 17:43:02 +0200

    fix: restore full sentry config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -22,10 +22,33 @@ const shouldSkipSentry = process.env.SKIP_SENTRY_BUILD === "true";
 export default shouldSkipSentry 
   ? nextConfig 
   : withSentryConfig(nextConfig, {
+      // For all available options, see:
+      // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
       org: "social-studio-pt",
       project: "social-studio",
+
+      // Only print logs for uploading source maps in CI
       silent: !process.env.CI,
-      widenClientFileUpload: false, // Disabled to save memory
+
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: false, // Set to false to save memory on 1GB VPS, true on Mac
+
+      // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+      // This can increase your server load as well as your hosting bill.
       tunnelRoute: "/monitoring",
-      // ... other options
+
+      webpack: {
+        // Enables automatic instrumentation of Vercel Cron Monitors.
+        automaticVercelMonitors: true,
+
+        // Tree-shaking options for reducing bundle size
+        treeshake: {
+          // Automatically tree-shake Sentry logger statements to reduce bundle size
+          removeDebugLogging: true,
+        },
+      },
     });
