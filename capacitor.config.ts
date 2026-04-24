edaa4cef1d4cf9af8e9250c03f6cf40f037c3dbd
thoufeeque <thoufeeque.rafique@gmail.com>
2026-04-25 00:56:43 +0200

    android/ios

---
+++ capacitor.config.ts
@@ -0,0 +1,25 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.thoufeeque.socialstudio',
+  appName: 'Social Studio',
+  webDir: 'out',
+  server: {
+    // IMPORTANT: Set this to your production URL to use the native shell
+    url: 'https://janet-composite-arbitrary-startup.trycloudflare.com',
+    cleartext: true,
+    allowNavigation: ['*']
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#0F172A",
+      showSpinner: false,
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: true,
+    }
+  }
+};
+
+export default config;
