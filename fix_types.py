483c4245f8bddefa54b61b265455f8ade73a9f15
thoufeeque <thoufeeque.rafique@gmail.com>
2026-08-28 08:24:11 +0200

    cleanup

--- fix_types.py
+++
@@ -1,8 +0,0 @@
-with open("src/lib/referral/retroactive.ts", "r") as f:
-    content = f.read()
-
-content = content.replace("import { Prisma } from '@prisma/client';", "import { prisma } from '@/lib/core/prisma';\ntype TxClient = Parameters<Parameters<typeof prisma.$transaction>[0]>[0];")
-content = content.replace("tx: Prisma.TransactionClient", "tx: TxClient")
-
-with open("src/lib/referral/retroactive.ts", "w") as f:
-    f.write(content)
